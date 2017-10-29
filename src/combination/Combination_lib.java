0482fe57602de6e9357b43a7d3c5202973e924e7
sho <sho@192.168.1.25>
2017-10-30 01:40:13 +0900

    Combination

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,17 @@
+package combination;
+
+public class Combination_lib {
+
+	public int getFactorial(int n) {
+		int i = 1;
+		int f = 1;
+		
+		while(i <= n) {
+			f*=i;
+			i++;
+		}
+		return f;
+		
+	}
+
+}
